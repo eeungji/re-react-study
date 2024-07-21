a49b009c531ce31a2c3282e568857259e4ae9cca
kimeunji <eunji93121@gmail.com>
2024-07-21 19:37:05 +0900

    react props2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import React from 'react';
-import Home from './components/RouteExample/pages/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout from './components/RouteExample/layout/RootLayout';
-import ErrorPage from './components/RouteExample/pages/ErrorPage';
-import Events, { loader as eventListLoader } from './components/RouteExample/pages/Events';
-import EventDetail, { loader as eventDetailLoader, action as deleteAction } from './components/RouteExample/pages/EventDetail';
-import EventLayout from './components/RouteExample/layout/EventLayout';
-import NewEvent from './components/RouteExample/pages/NewEvent';
-import EditPage from './components/RouteExample/pages/EditPage';
-import { action as manipulateAction } 
-  from './components/RouteExample/components/EventForm';
+import React from "react";
+import Home from "./components/RouteExample/pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/RouteExample/layout/RootLayout";
+import ErrorPage from "./components/RouteExample/pages/ErrorPage";
+import Events, {
+  loader as eventListLoader,
+} from "./components/RouteExample/pages/Events";
+import EventDetail, {
+  loader as eventDetailLoader,
+  action as deleteAction,
+} from "./components/RouteExample/pages/EventDetail";
+import EventLayout from "./components/RouteExample/layout/EventLayout";
+import NewEvent from "./components/RouteExample/pages/NewEvent";
+import EditPage from "./components/RouteExample/pages/EditPage";
+import { action as manipulateAction } from "./components/RouteExample/components/EventForm";
 
 const App = () => {
+
+  // 서버에서 지출항목 JSON 배열을 응답받음
+  const expenses = [
+  {
+    title: '치킨먹음',
+    price: 30000,
+    date: new Date(2024, 6 - 1, 3)
+  },
+  {
+    title: '족발먹음',
+    price: 40000,
+    date: new Date(2024, 6 - 1, 7)
+  },
+  {
+    title: '헬스장등록',
+    price: 300000,
+    date: new Date(2024, 6 - 1, 12)
+  },
+];
+
   return (
     <>
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem 
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
     </>
   );
-}
+};
 
 export default App;
-
-
-
-
 
 // // 라우터 설정
 // const router = createBrowserRouter([
@@ -38,33 +71,33 @@ export default App;
 //         path: 'events',
 //         element: <EventLayout />,
 //         children: [
-//           { 
-//             index: true, 
+//           {
+//             index: true,
 //             element: <Events />,
 //             // loader: eventListLoader,
 //           },
-//           { 
-//             path: ':eventId', 
+//           {
+//             path: ':eventId',
 //             loader: eventDetailLoader,
 //             // element: <EventDetail />,
 //             // loader가 children에게 직접적으로 연결되지 않아
 //             // EventDetail에서 loader를 사용하지 못하고 있음.
 //             id: 'event-detail', // loader에게 ID 부여
 //             children: [
-//               { 
-//                 index: true, 
+//               {
+//                 index: true,
 //                 element: <EventDetail />,
 //                 action: deleteAction
 //                },
-//               { 
+//               {
 //                 path: 'edit',
 //                 element: <EditPage />,
-//                 action: manipulateAction 
+//                 action: manipulateAction
 //               },
 //             ]
 //           },
-//           { 
-//             path: 'new', 
+//           {
+//             path: 'new',
 //             element: <NewEvent />,
 //             // 서버에 갱신데이터요청을 보낼 때 트리거
 //             action: manipulateAction
@@ -76,7 +109,7 @@ export default App;
 // ]);
 
 // const App = () => {
-  
+
 //   return (
 //     <RouterProvider router={router} />
 //   );
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -4,34 +4,40 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import { divide } from 'lodash';
 
+//aaa App.js 에서 객체로 받아옴
+const ExpenseItem = ({ date, title, price: exPrice}) => {
 
-const ExpenseItem = () => {
+  // console.log('props: ', aaa);
 
   // 변수 선언
-
-  const expenseDate = new Date(2024, 6, 12);
-  const expenseTitle = '점심식사';
-  const expensePrice= 8000;
+  // const expenseDate = date;
+  // const expenseTitle = title;
+  // const expensePrice= exPrice;
 
 
   // 함수 선언
-  const makeFormattedDate = () => {
+  // const makeFormattedDate = () => {
 
-    const year = expenseDate.getFullYear();
-    const month = expenseDate.getMonth();
-    const date = expenseDate.getDate();
+  //   const year = date.getFullYear();
+  //   const month = date.getMonth();
+  //   const day = date.getDate();
     
-    return `${year}년 ${month}월 ${date}일`;
+  //   return `${year}년 ${month}월 ${day}일`;
+  // };
+
+  // 1자리 숫자를 2자리수로 변환하는 함수
+  const make2digit = (text) => {
+    return text.toString().padStart(2, '0');
   };
 
   // 원화 표기법으로 변환
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(expensePrice);
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
 
   return (
     <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
+      <ExpenseDate />
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{formattedPrice}</div>
       </div>
     </div>
